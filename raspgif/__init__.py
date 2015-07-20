6dd1010ac791a61f58b33fc94c6324425212e154
Tomek Święcicki <tomislater@gmail.com>
2015-07-20 21:28:06 +0200

    Add constants

--- raspgif/__init__.py
+++ raspgif/__init__.py
@@ -1,1 +1,11 @@
 __version__ = '0.0.1'
+
+import constants
+
+
+def get_images():
+    pass
+
+
+def make_gif():
+    pass
---
+++ raspgif/constants.py
@@ -0,0 +1,9 @@
+RASP_URL = "http://rasp.linta.de/GERMANY/"
+suffix = "lst.d2.png"
+today = "curr"
+tomorrow = "curr+1"
+
+parameters = {
+    "Cu Potential": "zsfclcldif",
+    "Cu Cloudbase where Cu Potential>0": "zsfclclmask",
+}
